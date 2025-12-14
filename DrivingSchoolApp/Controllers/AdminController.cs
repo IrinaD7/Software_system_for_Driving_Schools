96c41423f49cbbc2da3094909d612651c62a20c1
irinka <irinadimakova05@yandex.ru>
2025-12-14 17:00:52 +0300

    Админ может создавать пользователей и роли

--- DrivingSchoolApp/Controllers/AdminController.cs
+++ DrivingSchoolApp/Controllers/AdminController.cs
@@ -1,14 +1,69 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using DrivingSchoolApp.Models.Admin;
+using Microsoft.AspNetCore.Identity;
 
 namespace DrivingSchoolApp.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            string password = "Test123!";
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                } 
+
+                return View(model);
+            }
+            
+            if(!await _roleManager.RoleExistsAsync(model.Role))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(model.Role));
+            }
+
+            await _userManager.AddToRoleAsync(user, model.Role);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ DrivingSchoolApp/Models/Admin/CreateUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models.Admin
+{
+	public class CreateUserViewModel
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string Role { get; set; }
+	}
+}
