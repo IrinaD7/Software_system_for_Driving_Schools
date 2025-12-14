41d865e81c11aa56cc38449633d03a1748dc397f
irinka <irinadimakova05@yandex.ru>
2025-12-14 18:23:13 +0300

    Учебная программа обновлена

--- DrivingSchoolApp/Models/StudyProgram.cs
+++ DrivingSchoolApp/Models/StudyProgram.cs
@@ -6,15 +6,19 @@ namespace DrivingSchoolApp.Models
 	{
 		public int Id { get; set; }
 
+		[Required(ErrorMessage = "Введите категорию")]
 		[Display(Name = "Категория обучения")]
 		public Category Category { get; set; }
 
+		[Required(ErrorMessage = "Введите количество часов")]
 		[Display(Name = "Количество теоретических часов")]
 		public int TheoryHours{ get; set; }
 
+		[Required(ErrorMessage = "Введите количество часов")]
 		[Display(Name = "Количество практических часов")]
 		public int PracticeHours { get; set; }
 
+		[Required(ErrorMessage = "Введите стоимость обучения")]
 		[Display(Name = "Стоимость обучения")]
 		public decimal Cost { get; set; }
 	}
