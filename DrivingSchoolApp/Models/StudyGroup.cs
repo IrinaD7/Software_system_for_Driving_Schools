bdec71291b2c153ccd5d278f1b5455fe7fb8eea0
irinka <irinadimakova05@yandex.ru>
2025-12-14 18:17:48 +0300

    Создана учебная группа и программа

---
+++ DrivingSchoolApp/Models/StudyGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class StudyGroup
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Введите название группы")]
+		[Display(Name = "Название группы")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Укажите учебную программу")]
+		[Display(Name = "Учебная программа")]
+		public StudyProgram StudyProgram { get; set; }
+
+		[Display(Name = "Дата начала обучения")]
+		public DateTime StartDate { get; set; }
+
+		[Display(Name = "Дата окончания обучения")]
+		public DateTime EndDate { get; set; }
+	}
+}
---
+++ DrivingSchoolApp/Models/StudyProgram.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class StudyProgram
+	{
+		public int Id { get; set; }
+
+		[Display(Name = "Категория обучения")]
+		public Category Category { get; set; }
+
+		[Display(Name = "Количество теоретических часов")]
+		public int TheoryHours{ get; set; }
+
+		[Display(Name = "Количество практических часов")]
+		public int PracticeHours { get; set; }
+
+		[Display(Name = "Стоимость обучения")]
+		public decimal Cost { get; set; }
+	}
+}
