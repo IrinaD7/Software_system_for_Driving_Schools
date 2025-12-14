dc947a1818e15551deb9bff1d2391d0c7a6192b4
irinka <irinadimakova05@yandex.ru>
2025-12-14 18:44:43 +0300

    Добавлено занятие по вождению

---
+++ DrivingSchoolApp/Models/DrivingLesson.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class DrivingLesson
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Укажите ученика")]
+		[Display(Name = "Ученик")]
+		public int StusentId { get; set; }	
+
+		public Student Student { get; set; }
+
+
+		[Required(ErrorMessage = "Укажите инструктора")]
+		[Display(Name = "Инструктор")]
+		public int InstructorId { get; set; }
+
+		public Instructor Instructor { get; set; }
+
+
+		[Required(ErrorMessage = "Укажите транспортное средство")]
+		[Display(Name = "Транспортное средство")]
+		public Vehicle Vehicle { get; set; }
+
+		[Required(ErrorMessage = "Укажите дату и время")]
+		[Display(Name = "Дата и время")]
+		public DateTime Date { get; set; }
+
+		[Range(1,5,ErrorMessage="Оценка должна быть от 1 до 5")]
+		[Display(Name = "Оценка")]
+		public int? Grade { get; set; }
+
+		[Display(Name = "Комментарий")]
+		public string Comment { get; set; }
+
+
+	}
+}
