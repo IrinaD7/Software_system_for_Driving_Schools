01c2440a0da1b1f0ec1612f0f6e0485b342fb52d
irinka <irinadimakova05@yandex.ru>
2025-12-26 10:53:36 +0300

    Lesson

---
+++ DrivingSchoolApp/Models/Lesson.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class Lesson
+	{
+		public int Id { get; set; }
+
+		[Display(Name = "Преподаватель")]
+		public int TeacherId { get; set; }
+
+		public Teacher Teacher{ get; set; }
+
+
+		[Display(Name = "Аудитория")]
+		public string Classroom{ get; set; }
+
+		[Display(Name = "Дата и время")]
+		public DateTime Date { get; set; }
+
+		[Display(Name = "Учебная группа")]
+		public StudyGroup Group { get; set; }
+	}
+}
