76999390e257f014f8c3d4541ab6be1cfda0962f
irinka <irinadimakova05@yandex.ru>
2025-12-26 10:32:25 +0300

    TheoryExam

---
+++ DrivingSchoolApp/Models/TheoryExam.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class TheoryExam : InternalExam
+	{
+		[Required]
+		[Display(Name = "Экзаменатор")]
+		public int TeacherId { get; set; }
+		public Teacher Teacher { get; set; }
+
+		[Required]
+		[Display(Name = "Время прохождения")]
+		public int Time {  get; set; }
+
+		[Required]
+		[Display(Name = "Правильные ответы")]
+		public int CorrectAnswers {  get; set; }
+
+		[Required]
+		[Display(Name = "Неправлильные ответы")]
+		public int WrongAnswer { get; set; }
+
+	}
+}
