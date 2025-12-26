334bead11d0069fb0d5c0e1dff9b9106fb72f6f1
irinka <irinadimakova05@yandex.ru>
2025-12-26 10:26:38 +0300

    InternalExam class

---
+++ DrivingSchoolApp/Models/InternalExam.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public abstract class InternalExam
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name = "Дата экзамена")]
+		public DateTime Date { get; set; }
+
+		[Required]
+		[Display(Name = "Студент")]
+		public int StudentId {  get; set; }
+
+		public Student Student { get; set; }
+
+
+
+		
+	}
+}
