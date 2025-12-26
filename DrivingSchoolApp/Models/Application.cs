d4f94898104899a313e4aac7691c760913ffeed5
irinka <irinadimakova05@yandex.ru>
2025-12-26 10:46:19 +0300

    Application

---
+++ DrivingSchoolApp/Models/Application.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class Application
+	{
+		[Required]
+		[Display(Name = "Статус")]
+		public Application Status { get; set; }
+
+		[Required]
+		[Display(Name = "Студент")]
+		public int StudentId { get; set; }
+
+		public Student Student { get; set; }
+
+		[Required]
+		[Display(Name = "Дата")]
+		public DateTime Date { get; set; }
+
+	}
+}
