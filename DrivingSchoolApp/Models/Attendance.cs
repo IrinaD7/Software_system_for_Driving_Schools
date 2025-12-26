d66cf8287e1d555c0c5fbc63163c832116e9dd51
irinka <irinadimakova05@yandex.ru>
2025-12-26 10:50:41 +0300

    Attendance

---
+++ DrivingSchoolApp/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrivingSchoolApp.Models
+{
+	public class Attendance
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name = "Студент")]
+		public int StudentId { get; set; }
+
+		public Student Student { get; set; }
+
+		[Required]
+		[Display(Name = "Занятие")]
+		public int LessonId { get; set; }
+
+		public  Lesson Lesson { get; set; }
+
+		public bool IsPresent { get; set; }
+	}
+}
